ad8fb3c895900613e03554716e3004d5f7c92dd8
Maria Jose Uceda Garcia <mj@Marias-Mac.local>
2018-01-28 05:43:35 -0800

    Initial Commit

---
+++ bakery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bakery
+//
+//  Created by Maria Jose Uceda Garcia on 28/01/2018.
+//  Copyright © 2018 Maria Jose Uceda Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
