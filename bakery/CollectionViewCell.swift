2c923ed3c8ccdfebbca6e749fc02c9b73fba5434
mjuceda <uceda@imac-de-dam.local>
2018-02-26 14:43:16 +0100

    login change button

---
+++ bakery/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  bakery
+//
+//  Created by mjuceda on 26/2/18.
+//  Copyright © 2018 Maria Jose Uceda Garcia. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    //
+    
+}
--- bakery/LoginViewController.swift
+++ bakery/LoginViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfUser: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
