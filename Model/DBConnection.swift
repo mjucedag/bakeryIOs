0995d0b165dc5651625f5f03ebe5a36109c03699
dam <migue66697@gmail.com>
2018-02-27 10:37:26 +0100

    Commit

---
+++ Model/DBConnection.swift
@@ -0,0 +1,58 @@
+import Foundation
+import UIKit
+
+class DBConnection{
+    
+    private let dbURL = "https://bakery-server-franor21.c9users.io/";
+    private var token:String = ""
+    
+    
+    
+    func getData(table: String, user: String, password: String, extra:String = "-1") -> Any{
+        var urlString = dbURL
+        urlString += table
+        guard let urlCon = URL(string: urlString) else {return "404"}
+        
+        let request = NSMutableURLRequest(url: urlCon)
+        
+        let data = "\(user):\(password)".data(using: .utf8)
+        let author = data!.base64EncodedString()
+        
+        request.setValue("Basic \(author)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        var products:[Product] = []
+        
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        }
+        
+        let colaGlobal = DispatchQueue.global()
+        colaGlobal.async {
+            defer{
+                DispatchQueue.main.async{
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                }
+            }
+            
+            URLSession.shared.dataTask(with: request as URLRequest){ (data, response, error) in
+                guard let data = data else{return}
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String:Any]
+                    print(json)
+                    self.token = json["t"] as? String ?? ""
+                    
+                    print("Token: \(self.token)")
+                    
+                    let results = json["r"] as? [[String:Any]] ?? nil
+                    results?.forEach{ row in
+                        products.append( Product(json: row)!)
+                    }
+                }catch {
+                    print("error")
+                }
+                }.resume()
+        }
+        return products
+    }
+}
--- bakery/LoginViewController.swift
+++ bakery/LoginViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var products:[Product] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let con = DBConnection()
 
+        self.products = con.getData(table: "product", user: "migue", password: "123") as? [Product] ?? []
+        
+        self.products.forEach { (product) in
+            print(product.description)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
