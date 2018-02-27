18bd2dcaaa93a374a8680a47724855c2cf00d862
mjuceda <uceda@imac-de-dam.local>
2018-02-27 14:41:43 +0100

    Medio Login

--- bakery/LoginViewController.swift
+++ bakery/LoginViewController.swift
@@ -12,11 +12,60 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var tfUser: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var error: UILabel!
+    
+    var products:[Product] = []
+    
+    var response:[String:Any] = [:]
+    
+    @IBAction func login(_ sender: UIButton) {
+        error.isHidden = true
+        error.text = ""
+        guard tfUser.text != "" && tfPassword.text != "" else{
+            
+            if tfUser.text == ""{
+                tfUser.layer.borderColor = UIColor.red.cgColor
+                tfUser.layer.borderWidth = 1.5
+                error.text! += "Fill your username \n"
+            }
+            if tfPassword.text == ""{
+                tfPassword.layer.borderColor = UIColor.red.cgColor
+                tfPassword.layer.borderWidth = 1.5
+                error.text! += "Fill your password"
+                
+            }
+            error.isHidden = false
+            return
+        }
+        tfUser.layer.borderColor = UIColor.white.cgColor
+        tfUser.layer.borderWidth = 1.0
+        
+        tfPassword.layer.borderColor = UIColor.white.cgColor
+        tfPassword.layer.borderWidth = 1.0
+
+        //hacer la conexion
+        let con = DBConnection()
+        let user = tfUser.text!
+        let password = tfPassword.text!
+        
+        self.products = con.getData(table: "product", user: user, password: password, {(x:Bool)->Void in
+            if x{
+                DispatchQueue.main.async {
+                    self.error.text = "Login Failed"
+                    self.error.isHidden = false
+                }
+            }else{
+                //Segue
+            }
+        }) as? [Product] ?? []
+    }
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
