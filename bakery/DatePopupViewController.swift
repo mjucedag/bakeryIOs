5a21391555a9d4258b2440d1b6965b79037f85c2
Maria Jose Uceda Garcia <mj@Marias-Mac.local>
2018-03-04 09:58:01 -0800

    interfaz consulta por dia (segment y datepicker)

---
+++ bakery/DatePopupViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DatePopupViewController.swift
+//  bakery
+//
+//  Created by Maria Jose Uceda Garcia on 04/03/2018.
+//  Copyright © 2018 Maria Jose Uceda Garcia. All rights reserved.
+//
+
+import UIKit
+
+class DatePopupViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var okButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func saveDate_TouchUpInside(_ sender: UIButton) {
+        
+        dismiss(animated: true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
