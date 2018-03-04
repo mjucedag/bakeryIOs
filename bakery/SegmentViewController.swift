ef717a57810b170fe51d3bb44ad5b7fba4e4cde9
Maria Jose Uceda Garcia <mj@Marias-Mac.local>
2018-03-04 12:24:38 -0800

    Add segment

---
+++ bakery/SegmentViewController.swift
@@ -0,0 +1,103 @@
+//
+//  SegmentViewController.swift
+//  bakery
+//
+//  Created by Maria Jose Uceda Garcia on 04/03/2018.
+//  Copyright © 2018 Maria Jose Uceda Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SegmentViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var mySegmentegControl: UISegmentedControl!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let dailyList:[String] = ["Private 1", "Private 2"] //tuplas de la consulta de tickets por dia
+    let memberTicketsList:[String] = ["Priv 1", "Priv 2"] //tuplas de la consulta de tickets por empleado
+    let familyTicketsList:[String] = ["aaaa 1", "aaa 2"] //tuplas de la consulta de tickets por familia
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var returnValue = 0
+        
+        switch(mySegmentegControl.selectedSegmentIndex){
+        case 0:
+            returnValue = dailyList.count
+            break
+        case 1:
+            returnValue = memberTicketsList.count
+            break
+        case 2:
+            returnValue = familyTicketsList.count
+            break
+        default:
+            break
+        }
+        return returnValue
+    
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        switch(mySegmentegControl.selectedSegmentIndex){
+        case 0:
+            myCell.textLabel!.text = dailyList[indexPath.row]
+            break
+        case 1:
+            myCell.textLabel!.text = memberTicketsList[indexPath.row]
+            break
+        case 2:
+            myCell.textLabel!.text = familyTicketsList[indexPath.row]
+            break
+        default:
+            break
+        }
+        return myCell
+    }
+
+    @IBAction func segmentedControlActionChanged(_ sender: Any) {
+        myTableView.reloadData()
+    }
+    
+    
+    @IBAction func showQueryPopup(_ sender: UIButton) {
+        
+        switch(mySegmentegControl.selectedSegmentIndex){
+        case 0:
+            let mainStoryboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let desVC = mainStoryboard.instantiateViewController(withIdentifier: "DatePopupViewController") as! DatePopupViewController
+            self.navigationController?.pushViewController(desVC, animated: true)
+            break
+        case 1:
+            //nada de momento
+            break
+        case 2:
+            //nada de momento
+            break
+        default:
+            break
+        }    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
